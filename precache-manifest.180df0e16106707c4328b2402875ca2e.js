b861675ed8a9a34e8a6bc5b3584a33ebff16a154
Anath <anathkantonda@gmail.com>
2020-06-03 21:27:49 +0200

    Updates

---
+++ precache-manifest.180df0e16106707c4328b2402875ca2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30a8ce19922e35854c3db4c01446a96",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "24818cfb7f5661d91594",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3015d106136eb6743401",
+    "url": "/robofriends/static/css/main.9252ac65.chunk.css"
+  },
+  {
+    "revision": "24818cfb7f5661d91594",
+    "url": "/robofriends/static/js/2.fa441a74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.fa441a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3015d106136eb6743401",
+    "url": "/robofriends/static/js/main.115cf41a.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
